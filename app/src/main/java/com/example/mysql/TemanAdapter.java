cf6ffc9c22d0c27e7b3ca280d8ca3a85b8c07132
Riefkha Ananda Putri <riefkhaanandaputri354@gmail.com>
2022-05-31 15:58:00 +0700

    memperbaiki codingan di TemanAdapter

--- app/src/main/java/com/example/mysql/TemanAdapter.java
+++ app/src/main/java/com/example/mysql/TemanAdapter.java
@@ -14,21 +14,10 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-//import com.example.sqlite.MainActivity;
-//import com.example.sqlite.R;
-//import com.example.sqlite.database.DBcontroller;
-//import com.example.sqlite.database.Teman;
-//import com.example.sqlite.edit_teman;
-
-//import com.example.sqlite.R;
-//import com.example.sqlite.database.Teman;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHolder> {
     private ArrayList<Teman> ListData;
-    private Context control;
 
     public TemanAdapter(ArrayList<Teman> listData) {
         this.ListData = listData;
@@ -38,7 +27,6 @@ public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHol
     public TemanAdapter.TemanViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutinf = LayoutInflater.from(parent.getContext());
         View view = layoutinf.inflate(R.layout.row_data_teman, parent, false);
-        control = parent.getContext();
         return new TemanViewHolder(view);
 
 
@@ -48,10 +36,8 @@ public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHol
     public void onBindViewHolder(TemanAdapter.TemanViewHolder holder, int position) {
         String nm, tlp, id;
 
-        id = ListData.get(position).getId();
         nm = ListData.get(position).getNama();
         tlp = ListData.get(position).getTelpon();
-        //DBcontroller db = new DBcontroller(control);
 
         holder.namaTxt.setTextColor(Color.BLUE);
         holder.namaTxt.setTextSize(20);
@@ -61,27 +47,7 @@ public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHol
         holder.cardku.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
-                PopupMenu popupMenu = new PopupMenu(control, holder.cardku);
-                //popupMenu.inflate(R.menu.menu);
-
-                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem Item) {
-                        switch (Item.getItemId()) {
-                            //case R.id.mnEdit:
-                                //Intent i = new Intent(control, edit_teman.class);
-                               // i.putExtra("id", id);
-                                //i.putExtra("nama", nm);
-                               // i.putExtra("telpon", tlp);
-                              ////  control.startActivity(i);
-                                break;
-
-                        }
-                        return false;
-                    }
-                });
-                popupMenu.show();
-                return false;
+                return true;
             }
         });
     }
